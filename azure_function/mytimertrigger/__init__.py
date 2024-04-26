bef2c4ea4d2411a9ed6ee2bd598ea9882afe725f
Roberto Schaefer <roschaeferjr@gmail.com>
2024-04-26 16:52:40 -0400

    azure_fuctions

--- azure_function/mytimertrigger/__init__.py
+++ azure_function/mytimertrigger/__init__.py
@@ -9,13 +9,7 @@ import azure.functions as func # type: ignore
 
 import logging
 
-blueprint = func.Blueprint()
-
-
-@blueprint.timer_trigger(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
-def timer_trigger(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
+def main(mytimer: func.TimerRequest) -> None:
+    if mytimer.past_due:
         logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function executed.')
+    logging.info('Timer trigger function ran at midnight')
